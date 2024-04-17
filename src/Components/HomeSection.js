7eac6f46369f7e11a652b3c12a4f8d1d961feb34
SuhailSakhi <suhail_909@outlook.com>
2024-04-17 17:58:47 +0200

    6e commit

--- src/Components/HomeSection.js
+++ src/Components/HomeSection.js
@@ -6,7 +6,7 @@ const HomeSection = ({ trail, namePosition }) => {
             <div className="max-w-7xl mx-auto px-4" style={{ left: `${namePosition.x}px`, top: `${namePosition.y}px` }}>
                 <div className="text-center">
                     <h1 className="text-7xl font-bold flex items-center justify-center">
-                        Welcome <img className="w-12 h-auto ml-2" src="/img/Wave.png" alt="wave" />
+                        Welcome <img className="w-12 h-auto ml-2" src="/img/wave.png" alt="wave" />
                     </h1>
                     <h2 className="text-4xl font-bold">I am  <span className="text-blue-500">Suhail Sakhizadh</span></h2>
                     <p className="mt-4 text-xl font-medium">Student Creative Technologist</p>
@@ -18,7 +18,7 @@ const HomeSection = ({ trail, namePosition }) => {
                     <img
                         key={index}
                         className="absolute"
-                        src="/img/Wave.png"
+                        src="/img/wave.png"
                         alt="logo"
                         style={{
                             width: '15px',
