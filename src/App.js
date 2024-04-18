d87bac4ac2129e5c354ec38046bdbc64c1932580
SuhailSakhi <suhail_909@outlook.com>
2024-04-18 18:09:34 +0200

    7e commit

--- src/App.js
+++ src/App.js
@@ -4,28 +4,47 @@ import './tailwind.css';
 function App() {
     const [trail, setTrail] = useState([]);
     const [mouseStopped, setMouseStopped] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
     useEffect(() => {
-        const handleMouseMove = (e) => {
-            const trailLength = 8;
-            const newTrail = [{ x: e.clientX, y: e.clientY }];
-            setTrail((prevTrail) => {
-                return [...prevTrail.slice(-trailLength), ...newTrail];
-            });
-            setMouseStopped(false); // Reset de timer wanneer de muis beweegt
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 640); // Define your breakpoint here
         };
 
-        document.addEventListener('mousemove', handleMouseMove);
+        handleResize(); // Initial check
 
-        const timer = setTimeout(() => {
-            setMouseStopped(true); // Zet mouseStopped op true nadat de muis stopt met bewegen
-        }, 1000); // Tijd in milliseconden, hier na 1 seconde
+        window.addEventListener('resize', handleResize);
 
-        return () => {
-            document.removeEventListener('mousemove', handleMouseMove);
-            clearTimeout(timer); // Verwijder de timer wanneer het component wordt uitgerend
-        };
+        return () => window.removeEventListener('resize', handleResize);
     }, []);
+
+    useEffect(() => {
+        if (!isSmallScreen) {
+            const handleMouseMove = (e) => {
+                const trailLength = 8;
+                const newTrail = [{ x: e.clientX, y: e.clientY }];
+                setTrail((prevTrail) => {
+                    return [...prevTrail.slice(-trailLength), ...newTrail];
+                });
+                setMouseStopped(false);
+            };
+
+            document.addEventListener('mousemove', handleMouseMove);
+
+            const timer = setTimeout(() => {
+                setMouseStopped(true);
+            }, 1000);
+
+            return () => {
+                document.removeEventListener('mousemove', handleMouseMove);
+                clearTimeout(timer);
+            };
+        } else {
+            // Clear the trail on small screens
+            setTrail([]);
+        }
+    }, [isSmallScreen]);
+
 
     useEffect(() => {
         if (mouseStopped) {
@@ -55,7 +74,7 @@ function App() {
             <nav className="bg-black text-white h-16 flex items-center fixed w-full top-0 z-50">
                 <div>
                     <img
-                        className="h-12" src="/img/logo.png" alt="logo"
+                        className="h-10 w-auto" src="/img/logo.png" alt="logo"
                         onClick={(e) => {
                             e.preventDefault(); // Voorkom standaardgedrag van de link
                             scrollToSection(homeRef, e); // Scroll naar de home-sectie
@@ -78,13 +97,13 @@ function App() {
             </nav>
 
             {/* Naam Section met Animatie */}
-            <section id="home" ref={homeRef} className="bg-black text-white py-40 relative mt-16 h-screen">
+            <section id="home" ref={homeRef} className={`bg-black text-white py-20 sm:py-40 relative mt-16 sm:mt-0 h-screen ${isSmallScreen ? 'text-lg' : ''}`}>
                 <div className="max-w-7xl mx-auto px-4">
                     <div className="text-center">
-                        <h1 className="text-7xl font-bold flex items-center justify-center">
+                        <h1 className="text-9xl font-bold flex items-center justify-center">
                             Welcome <img className="w-12 h-auto ml-2" src="/img/wave.png" alt="wave" />
                         </h1>
-                        <h2 className="text-4xl font-bold">I am  <span className="text-blue-500">Suhail Sakhizadh</span></h2>
+                        <h2 className="text-5xl font-bold">I am  <span className="text-blue-500">Suhail Sakhizadh</span></h2>
                         <p className="mt-4 text-xl font-medium">Student Creative Technologist</p>
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
@@ -112,13 +131,13 @@ function App() {
             </section>
 
             {/* Over mij Section */}
-            <section id="about" ref={aboutRef} className="py-40 bg-white">
+            <section id="about" ref={aboutRef} className="py-20 sm:py-60 bg-white">
                 <div className="max-w-7xl mx-auto px-4 flex items-center">
                     <div className="w-1/2">
                         <img className="w-80 h-80 object-cover rounded-full" src="/img/pf.jpg" alt="Profielfoto" />
                     </div>
                     <div className="w-1/2 px-6">
-                        <h2 className="text-3xl font-bold text-gray-800 mb-4">About Me</h2>
+                        <h2 className="text-5xl font-bold text-gray-800 mb-4">About Me</h2>
                         <p className="text-lg text-blue-500">
                             "As a creative developer, I am more than just focused on code. My differentiating factor lies in my ability to come up with out-of-the-box ideas and to research and engage with the target audience in the development process. This enables me to create the most optimal product."                        </p>
                     </div>
@@ -126,10 +145,10 @@ function App() {
             </section>
 
             {/* skills section */}
-            <section id="skills" ref={skillsRef} className="py-20 bg-gray-100">
+            <section id="skills" ref={skillsRef} className="py-20 px-4 sm:px-20 bg-gray-100">
                 <div className="max-w-7xl mx-auto px-20 flex items-center">
                     <div className="w-1/2 px-15">
-                        <h2 className="text-3xl font-bold text-gray-800 mb-4">My Skills</h2>
+                        <h2 className="text-5xl font-bold text-gray-800 mb-4">My Skills</h2>
                         <p className="text-lg text-blue-500">
                             "I have some experience in various aspects of web development and design. My expertise encompasses a wide range of programming languages, frameworks, and design skills. Additionally, I possess problem-solving skills and have experience with project management such as Agile and Scrum. My passion for creativity enables me to devise innovative solutions and elevate my work to a higher level. I am currently seeking a position at a company where I can continue to learn and grow professionally."
                         </p>
@@ -138,7 +157,6 @@ function App() {
                         <img className="w-64 h-64 aspect-square profielfoto" src="/img/memoji2.png" alt="Profielfoto" />
                     </div>
                 </div>
-                <br/>
                 <div className="w-1/2 flex justify-center items-center skill-icons-container">
                     <p className="text-lg text-blue-500">
                         "I have worked with the following technologies:"
@@ -163,15 +181,15 @@ function App() {
 
 
             {/* Projecten Section */}
-            <section id="projects" ref={projectsRef} className="bg-gray-100 py-32">
+            <section id="projects" ref={projectsRef} className="bg-gray-100 py-20 sm:py-40">
                 <div className="max-w-7xl mx-auto px-4">
-                    <h2 className="text-3xl font-bold text-gray-800 mb-8 text-center">My favorite projects</h2>
+                    <h2 className="text-5xl font-bold text-gray-800 mb-8 text-center">My favorite projects</h2>
                     <div className="flex flex-wrap justify-center">
                         {/* Projectkaart 1 */}
                         <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-                            <img className="w-full" src="/img/project1.jpg" alt="Project 1" />
                             <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2">Project 1</div>
+                                <img className="w-full" src="/img/project1.jpg" alt="Project 1" />
+                                <div className="font-bold text-xl mb-2">Country Trivia (AI) </div>
                                 <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </div>
                             <div className="px-6 pt-4 pb-2">
@@ -182,17 +200,17 @@ function App() {
                         <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
                             <img className="w-full" src="/img/project2.jpg" alt="Project 2" />
                             <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2">Project 2</div>
-                                <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                <div className="font-bold text-xl mb-2">Studate</div>
+                                <p className="text-gray-700 text-base">Back end focussed project where i built a student meeting website where you can make an own account and like other accounts.</p>
                             </div>
                             <div className="px-6 pt-4 pb-2">
                             </div>
                         </div>
                         {/* Projectkaart 3 */}
                         <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-                            <img className="w-full" src="/img/project3.jpg" alt="Project 3" />
                             <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2">Project 3</div>
+                                <img className="w-full" src="/img/project3.jpg" alt="Project 3" />
+                                <div className="font-bold text-xl mb-2">Coming soon (Neural Network)</div>
                                 <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </div>
                             <div className="px-6 pt-4 pb-2">
